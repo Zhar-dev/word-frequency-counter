16ed1ba00602029525fff8f994abe449cf6b58e2
Christian Clavite <156822259+XTian-Clav@users.noreply.github.com>
2024-03-08 15:04:13 +0800

    added link in index.php

--- index.php
+++ index.php
@@ -63,6 +63,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
   <meta charset="UTF-8">
   <title>Word Frequency Counter</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
   <h1>Word Frequency Counter</h1>
